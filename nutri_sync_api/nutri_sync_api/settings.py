2dd8e9186a9269f14d6072edd1b093a6982267d1
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-01-30 18:13:20 -0300

    configuraçao do cors

--- nutri_sync_api/nutri_sync_api/settings.py
+++ nutri_sync_api/nutri_sync_api/settings.py
@@ -38,16 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    # Adicione mais origens permitidas, se necessário
 ]
 
 ROOT_URLCONF = 'nutri_sync_api.urls'
