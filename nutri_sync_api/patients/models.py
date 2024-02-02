9d70b1891de86779984a27e1539a7ae2807e7511
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-02-02 20:48:21 -0300

    patients

--- nutri_sync_api/patients/models.py
+++ nutri_sync_api/patients/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
 
-class Users(models.Model):
+class Patients(models.Model):
     username = models.CharField(max_length=150)
     name = models.CharField(max_length=150)
-    email = models.EmailField(unique=True)
+    email = models.CharField(unique=True)
     password = models.CharField(max_length=128)
+    age = models.IntegerField()
+    dateOfBirth = models.DateField()
+    address = models.CharField()
+    occupation = models.CharField()
+    obs = models.CharField()
 
     class Meta:
-        db_table = 'Users'
-
+        db_table = 'Patients'
+    
     def save(self, *args, **kwargs):
         if self.pk is None or 'password' in kwargs:
             self.password = make_password(self.password)
 
-        super().save(*args, **kwargs)    
+        super().save(*args, **kwargs)
