c6953b289919cd7d0cccb9d3adf07a6b6b05ee9d
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-02-05 23:53:15 -0300

    cria filtro nutri id e name

--- nutri_sync_api/patients/views.py
+++ nutri_sync_api/patients/views.py
@@ -3,10 +3,24 @@ from .models import Patients
 from .serializers import PatientsSerializer
 from rest_framework.response import Response
 from rest_framework import generics, status
+from django.db.models import Q
 
 class PatientsList(generics.ListCreateAPIView):
-    queryset = Patients.objects.all()
     serializer_class = PatientsSerializer
+
+    def get_queryset(self):
+        nutri_id = self.request.query_params.get('nutriID', None)
+        name = self.request.query_params.get('name', None)
+
+        queryset = Patients.objects.all()
+
+        if nutri_id:
+            queryset = queryset.filter(nutriID=nutri_id)
+
+        if name:
+            queryset = queryset.filter(Q(name__icontains=name) | Q(name=''))
+
+        return queryset
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -14,3 +28,5 @@ class PatientsList(generics.ListCreateAPIView):
         self.perform_create(serializer)
 
         return Response({'detail': 'Paciente criado com sucesso.'}, status=status.HTTP_201_CREATED)
+
+
