798fd94a630dcc7eaf021f9f106d35f058d9f45f
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-03-19 20:24:15 -0300

    iniciando model

--- nutri_sync_api/bioimpedance/models.py
+++ nutri_sync_api/bioimpedance/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Bioimpedance(models.Model):
+    nutriId = models.BigIntegerField()
+    patientId = models.BigIntegerField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    bmi = models.DecimalField(max_digits=5, decimal_places=2)
+    bodyFat = models.DecimalField(max_digits=5, decimal_places=2)
+    muscleRate = models.DecimalField(max_digits=5, decimal_places=2)
+    fatFreeDough = models.DecimalField(max_digits=5, decimal_places=2)
